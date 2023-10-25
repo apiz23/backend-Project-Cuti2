e88287b866f727d1617cba5773b556b9f4d3ca68
Hafizuddin <piz230601@gmail.com>
2023-10-25 21:15:28 +0800

    testing json server

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 
 const app = express();
 const home = require("./routes/home");
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/home", home);
-// const jsonServerMiddleware = jsonServer.router("./data/db.json");
-// app.use("/api", jsonServerMiddleware);
+
+const jsonServerMiddleware = jsonServer.router("./data/db.json");
+app.use("/api", jsonServerMiddleware);
+
 const port = process.env.PORT || 5002;
 app.listen(port, () => console.log(`Listening to port ${port}`));
