c14025b5a5f85edfc5dbe54cf80e16269b92acdf
Hafizuddin <piz230601@gmail.com>
2023-10-25 21:08:52 +0800

    fix

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 
 const app = express();
-
+const home = require("./routes/home");
 app.use(cors());
-const jsonServerMiddleware = jsonServer.router("./data/db.json");
-app.use("/api", jsonServerMiddleware);
+app.use(express.json());
 
-app.listen(5000, () => {
-	console.log("Server is running on http://localhost:5000");
-});
+app.use("/home", home);
+// const jsonServerMiddleware = jsonServer.router("./data/db.json");
+// app.use("/api", jsonServerMiddleware);
+const port = process.env.PORT || 5002;
+app.listen(port, () => console.log(`Listening to port ${port}`));
